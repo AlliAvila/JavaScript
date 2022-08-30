8800d4da7492f6d0e7c064388b0722030b71d700
unknown <Allison@MSI-A01338.mps.k12>
2022-08-30 12:58:14 -0700

    Bees

--- JS-BASICS/index.js
+++ JS-BASICS/index.js
@@ -1,2 +1,21 @@
-//This is my first JavaScript code!
-console.log('Hello World');c
+  //This is my first JavaScript code!
+  console.log('Hello World');
+
+let name = 'Allison';  //String literal
+let age = 15; //number literal
+let isApproved = true; //Boolean literal
+let firstName = undefined;
+let selectedColor = null;
+console.log(name);
+
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name)
+//Cannot containn a space or hypen (-)
+
+let firstName = 'Allison';
+let lastName = 'Avila';
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
