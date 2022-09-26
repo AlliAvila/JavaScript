c4fd887a8fde7b9600dd76781bcc22eb1df53901
unknown <Allison@MSI-A01338.mps.k12>
2022-09-26 12:59:51 -0700

    lll

---
+++ JS-BASICS/minmax.js
@@ -0,0 +1,14 @@
+function minMax(arr){
+    let minNum=arr[0];
+    for (let i=0; i<arr.length; i++){
+       if(arr[i]<minNum){
+        minNum=arr[i]
+       }
+    }
+    console.log ("minimum number is: " + minNum)
+    return minNum
+}
+
+minMax([9,5,23,1,-4.22,13,64,-13])
+
+
