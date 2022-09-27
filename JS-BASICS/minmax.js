30760591444eca65834ddbc0b0c384ab7d64a722
unknown <Allison@MSI-A01338.mps.k12>
2022-09-27 12:59:01 -0700

    sdd

--- JS-BASICS/minmax.js
+++ JS-BASICS/minmax.js
@@ -5,10 +5,18 @@ function minMax(arr){
         minNum=arr[i]
        }
     }
-    console.log ("minimum number is: " + minNum)
-    return minNum
+    let maxNum=arr[0];
+    for (let i=0; i<arr.length; i++){
+       if(arr[i]>maxNum){
+          maxNum=arr[i]
+       }
+    }
+    let resultArray[];
+    resultArray[0]=minNum
+    resultArray[1]=maxNum
+    return (resultArray)
 }
 
-minMax([9,5,23,1,-4.22,13,64,-13])
+minMax([9,5,23,1,-4.234,5,76,-334234,5,7,8556,345,7])
 
 
